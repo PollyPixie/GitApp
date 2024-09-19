57eb34ed45f66114e44db151d085fe2613d0a8b7
Polina Sokolova <polly.sokolova@icloud.com>
2024-09-20 00:10:20 +0400

    Set up UIButton

--- GitApp/GitApp/ViewController.swift
+++ GitApp/GitApp/ViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     private let helper = Helper()
     private let userRepository = UserRepository()
+    
     private let textLabel = UILabel()
+    private let textButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show FullName", for: .normal)
+        button.backgroundColor = .green
+        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +28,7 @@ class ViewController: UIViewController {
         
         setupUILabel()
         view.addSubview(textLabel)
+        view.addSubview(textButton)
     }
         
     private func setupUser() {
